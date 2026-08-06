4f2f5af4aa777738dd7948b0c2b7b5b279d89e61
Benjamin Krug <benjaminkrug@yahoo.com>
2026-08-05 22:17:19 -0400

    Deserialize response

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from planning_center_client import PlanningCenterClient
 
 httpClient = PlanningCenterClient()
-httpClient.get_people(3017831)
+people_data = httpClient.get_people(3017831)
+
+print(len(people_data.data))
+print(people_data.meta.count)
+print(people_data.meta.total_count)
--- planning_center_client.py
+++ planning_center_client.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+from models import GroupPeopleGetResponse
 
 load_dotenv()
 
@@ -10,7 +11,7 @@ class PlanningCenterClient:
         self.api_client_id = os.getenv("PLANNING_CENTER_CLIENT_ID")
         self.api_secret = os.getenv("PLANNING_CENTER_SECRET")
 
-    def get_people(self, group_id: int) -> None:
+    def get_people(self, group_id: int) -> GroupPeopleGetResponse:
         print("got here")
         url = f"https://api.planningcenteronline.com/groups/v2/groups/{group_id}/people?per_page=3"
         response = requests.get(url, auth=(self.api_client_id, self.api_secret))
@@ -18,3 +19,5 @@ class PlanningCenterClient:
         print(response.status_code)
         with open("people_response.txt", "w") as f:
             f.write(response.text)
+
+        return GroupPeopleGetResponse.model_validate_json(response.text)
