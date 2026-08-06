459b677d05dc959eb95e2de46879ccac41d6590c
Benjamin Krug <benjaminkrug@yahoo.com>
2026-08-05 21:30:10 -0400

    Make GET request to Planning Center

---
+++ main.py
@@ -0,0 +1,4 @@
+from planning_center_client import PlanningCenterClient
+
+httpClient = PlanningCenterClient()
+httpClient.get_people(3017831)
--- planning_center_client.py
+++ planning_center_client.py
@@ -5,14 +5,16 @@ import os
 load_dotenv()
 
 class PlanningCenterClient:
-    def __init__(self, s):
-        print("inside the simple constructor")
+    def __init__(self):
+        # Raise an error if either of these values is blank
         self.api_client_id = os.getenv("PLANNING_CENTER_CLIENT_ID")
         self.api_secret = os.getenv("PLANNING_CENTER_SECRET")
 
     def get_people(self, group_id: int) -> None:
+        print("got here")
         url = f"https://api.planningcenteronline.com/groups/v2/groups/{group_id}/people?per_page=3"
         response = requests.get(url, auth=(self.api_client_id, self.api_secret))
 
+        print(response.status_code)
         with open("people_response.txt", "w") as f:
             f.write(response.text)
