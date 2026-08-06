f825e19ec89554ce52c1dd7425e3be53bcf09235
Benjamin Krug <benjaminkrug@yahoo.com>
2026-08-05 21:11:52 -0400

    Use secrets in HTTP requests

--- planning_center_client.py
+++ planning_center_client.py
@@ -4,13 +4,15 @@ import os
 
 load_dotenv()
 
-api_client_id = os.getenv("PLANNING_CENTER_CLIENT_ID")
-api_secret = os.getenv("PLANNING_CENTER_SECRET")
-
 class PlanningCenterClient:
+    def __init__(self, s):
+        print("inside the simple constructor")
+        self.api_client_id = os.getenv("PLANNING_CENTER_CLIENT_ID")
+        self.api_secret = os.getenv("PLANNING_CENTER_SECRET")
+
     def get_people(self, group_id: int) -> None:
         url = f"https://api.planningcenteronline.com/groups/v2/groups/{group_id}/people?per_page=3"
-        response = requests.get(url)
+        response = requests.get(url, auth=(self.api_client_id, self.api_secret))
 
         with open("people_response.txt", "w") as f:
             f.write(response.text)
