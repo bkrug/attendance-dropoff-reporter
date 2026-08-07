e6bb7769e56243d30e1845753da95e6c219f946c
Benjamin Krug <benjaminkrug@yahoo.com>
2026-08-06 21:37:00 -0400

    Use total count instead of len()

--- src/attendance_decline_accumulator.py
+++ src/attendance_decline_accumulator.py
@@ -10,16 +10,18 @@ class AttendanceDeclineAccumulator:
     
     def get_members_with_declining_attendance(self, group_name: str) -> DeclineReport:
         group_response = self.http_client.get_group(group_name)
-        if len(group_response.data)==0:
+        if group_response.meta.total_count==0:
             return DeclineReport("Group not found", [])
 
         group_id = group_response.data[0].id
         events_response = self.http_client.get_events(group_id, '1900-01-01', '1901-01-01', 0, 25)
-        if len(events_response.data)==0:
+        if events_response.meta.total_count==0:
             return DeclineReport("Group has no events (worship services)", [])
 
         people_response = self.http_client.get_people(group_id, 0, 25)
-        if len(people_response.data)==0:
+        if people_response.meta.total_count==0:
             return DeclineReport("Group has no people", [])
+
+
 
         return DeclineReport(None, [])
