3d3a762d310c3ebd07ad416039a98b75f0dd3d89
Benjamin Krug <benjaminkrug@yahoo.com>
2026-08-14 17:32:51 -0400

    Add "global" exception handling

--- function_app.py
+++ function_app.py
@@ -6,6 +6,7 @@ import azure.functions as func
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "src"))
 
 from report_orchastrator import ReportOrchastrator
+from report_email_sender import ReportEmailSender
 
 app = func.FunctionApp()
 
@@ -15,5 +16,19 @@ def AttendanceTimedReport(myTimer: func.TimerRequest) -> None:
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
-    logging.info('Python timer trigger function executed.')
-    ReportOrchastrator().generate_report()
+    try:
+        logging.info('Python timer trigger function executed.')
+        ReportOrchastrator().generate_report()
+    except Exception as ex:
+        _handle_failure(ex)
+
+def _handle_failure(ex: Exception) -> None:
+    logging.critical("Unhandled exception in AttendanceTimedReport", exc_info=ex)
+
+    try:
+        recipients = os.getenv("REPORT_EMAIL_RECIPIENTS", "")
+        if recipients:
+            email_msg = "An uncaught exception occurred when attempting to generate an attendance report: " + str(ex)
+            ReportEmailSender().send_error(email_msg, recipients)
+    except Exception as email_ex:
+        logging.critical("Failed to send failure notification email", exc_info=email_ex)
